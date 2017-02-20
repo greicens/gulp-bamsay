10143a4e30c22e9b672b3dad1a422c3f1611f1c8
greicens <gsilva@mail.ccsf.edu>
2017-02-20 11:15:45 -0800

    adds gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,25 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var sass = require("gulp-sass");
+
+gulp.task('default', function() {
+  console.log('I am the default task!');
+});
+
+gulp.task('currenttime', function(){
+  var today = new Date()
+  console.log(today)
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('./js/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('sass', function () {
+return gulp.src('./css/**/*.scss')
+ .pipe(sass().on('error', sass.logError))
+ .pipe(gulp.dest('./css/'))
+});
